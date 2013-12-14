1e650631eae6a4a18bc62a10575d8f396c4ddc97
Sergiovb.Inf@gmail.com <Sergiovb.Inf@gmail.com@15079975-0d8f-49b9-43ae-de39589eff96>
2013-12-14 13:04:36 +0000

    Añadidas clases actores y primera versión de personalización

--- EsqueletoFiboo-desktop/src/es/uca/fiboo/Main.java
+++ EsqueletoFiboo-desktop/src/es/uca/fiboo/Main.java
@@ -8,8 +8,8 @@ public class Main {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "EsqueletoFiboo";
 		cfg.useGL20 = false;
-		cfg.width = 480;
-		cfg.height = 320;
+		cfg.width = 1280;
+		cfg.height = 800;
 		
 		new LwjglApplication(new fibooGame(), cfg);
 	}
