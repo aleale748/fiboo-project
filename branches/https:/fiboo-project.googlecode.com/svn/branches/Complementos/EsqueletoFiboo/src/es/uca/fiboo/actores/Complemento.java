e655c6b9351a3f1087b0248255e58ec0cfa4df7e
Sergiovb.Inf@gmail.com <Sergiovb.Inf@gmail.com@15079975-0d8f-49b9-43ae-de39589eff96>
2013-12-15 18:09:12 +0000

    Cambio de estructura de complementos

---
+++ branches/https:/fiboo-project.googlecode.com/svn/branches/Complementos/EsqueletoFiboo/src/es/uca/fiboo/actores/Complemento.java
@@ -0,0 +1,42 @@
+package es.uca.fiboo.actores;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+public class Complemento extends Image {
+	
+	public static enum Tipo {
+		OJOS, PELO, GAFAS, DISFRAZ, CAMISA, PANTALON, MASCARA;
+	}
+	
+	private Tipo tipo;
+	private Rectangle bounds;
+	
+	public Complemento(Texture imagen, Tipo tipo) {
+		super(imagen);
+		this.tipo = tipo;
+		this.bounds = new Rectangle(getX(), getY(), imagen.getWidth(), imagen.getHeight());
+	}
+	
+	public Tipo getTipo() {
+		return tipo;
+	}
+	
+	public boolean isTipo(Tipo tipo) {
+		if(tipo == this.tipo)
+			return true;
+		return false;
+	}
+	
+	public Rectangle getBounds() {
+		return bounds;
+	}
+	
+	@Override
+	public void setPosition(float x, float y) {
+		super.setPosition(x, y);
+		bounds.setPosition(x, y);
+	}
+	
+}
