a0f0b41ccb3efa9165ae24c09edc0217308f778b
ismael.jerezibanez@gmail.com <ismael.jerezibanez@gmail.com@15079975-0d8f-49b9-43ae-de39589eff96>
2013-12-23 12:31:17 +0000

    Disminuido el nivel de dificultad: - El asteroide solución se muestra siempre. - Hay más separación aleatoria entre los asteroides.

--- trunk/EsqueletoFiboo/src/es/uca/fiboo/screens/MainScreen.java
+++ trunk/EsqueletoFiboo/src/es/uca/fiboo/screens/MainScreen.java
@@ -12,7 +12,9 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Scaling;
+
 import es.uca.fiboo.fibooGame;
+import es.uca.fiboo.naveminigame.screens.NaveMiniGameScreen;
 
 public class MainScreen extends AbstractScreen {
 
