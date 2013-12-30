5db7e87d2e9c43609ef247f2666170e4b40fe954
wenceslaosanchezpino@gmail.com <wenceslaosanchezpino@gmail.com@15079975-0d8f-49b9-43ae-de39589eff96>
2013-12-30 11:07:41 +0000

    Version 0.1 del minijuego de la camara ortografica

---
+++ trunk/EsqueletoFiboo/src/es/uca/fiboo/Fondo.java
@@ -0,0 +1,23 @@
+package es.uca.fiboo.camera;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+
+public class Fondo extends Actor {
+	
+	Texture texture = new Texture(Gdx.files.internal("parque.png"));
+	
+	@Override
+	public void draw(SpriteBatch batch, float alpha){
+		batch.draw(texture,0,0);
+	}
+	
+	public boolean touchDown(InputEvent event, float x, float y, int pointer,
+			int button) {
+		Gdx.app.log(Makipong.LOG, "Screen: x=" + x + "; y=" + y);
+		return true;
+	}
+}
