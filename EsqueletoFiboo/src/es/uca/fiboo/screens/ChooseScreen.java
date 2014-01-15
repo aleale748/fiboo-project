1b1b3b097db852d3e4658bad3eb27d5912b7b785
Sergiovb.Inf@gmail.com <Sergiovb.Inf@gmail.com@15079975-0d8f-49b9-43ae-de39589eff96>
2014-01-15 03:58:36 +0000

    Mejoras en menus.

--- EsqueletoFiboo/src/es/uca/fiboo/screens/ChooseScreen.java
+++ EsqueletoFiboo/src/es/uca/fiboo/screens/ChooseScreen.java
@@ -6,104 +6,89 @@ import static com.badlogic.gdx.scenes.scene2d.actions.Actions.sequence;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 import es.uca.fiboo.fibooGame;
 
 public class ChooseScreen extends AbstractScreen {
 
-	private ImageButton ninoBoton;
-	private ImageButton ninaBoton;
+	private Image ninoBoton;
+	private Image ninaBoton;
 	private Image imgFondo;
-	float w,h;
+	private float w,h;
+	
 	public ChooseScreen(fibooGame game) {
 		super(game);
 		Gdx.input.setInputProcessor(stage);
+		
 		imgFondo = new Image(fibooGame.MANAGER.get("robotgame/fondoestrellas.png", Texture.class));
 		imgFondo.setFillParent(true);
 		stage.addActor(imgFondo);
-		w= Gdx.graphics.getWidth();
-		h= Gdx.graphics.getHeight();
+		w = Gdx.graphics.getWidth();
+		h = Gdx.graphics.getHeight();
 	}
 
 	@Override
 	public void show() {
 		super.show();
-
-		TextureRegion ninoBotonRegion = new TextureRegion(fibooGame.MANAGER.get("portada/polaroidnino2.png", Texture.class));
-		TextureRegion ninaBotonRegion = new TextureRegion(fibooGame.MANAGER.get("portada/polaroidnina2.png", Texture.class));
-		Drawable ninoBotonDrawable = new TextureRegionDrawable(ninoBotonRegion);
-		Drawable ninaBotonDrawable = new TextureRegionDrawable(ninaBotonRegion);
-
-		// Creamos el boton, lo posicionamo y lo a�adimo al stage
-		ninoBoton = new ImageButton(ninoBotonDrawable);
-		ninaBoton = new ImageButton(ninaBotonDrawable);
+		
+		Texture nino = fibooGame.MANAGER.get("portada/polaroidnino2.png", Texture.class);
+		Texture nina = fibooGame.MANAGER.get("portada/polaroidnina2.png", Texture.class);
+		nino.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+		nina.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+		ninoBoton = new Image(nino);
+		ninaBoton = new Image(nina);
 		
 		float imgWidth = w * 0.3f;
 		float imgHeight = imgWidth;
 		ninoBoton.setSize(imgWidth, imgHeight);
 		ninaBoton.setSize(imgWidth, imgHeight);		
 		
-		ninoBoton.setX( w / 4f - ninoBoton.getWidth() / 2f);
-		ninoBoton.setY(h / 2f - ninoBoton.getHeight() / 2f);
-		ninoBoton.addListener(new InputListener() {
-			@Override
-			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-				Gdx.app.log(fibooGame.LOG, "Touching down on ninoBoton");
-				return true;
-			}
-			@Override
-			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-				Gdx.app.log(fibooGame.LOG, "Touching up on ninoBoton");
-				fibooGame.getPersonaje().getAvatar().setBase("pelonino3");
-				imgFondo.addAction( sequence(delay(1.0f), fadeOut(0.75f)));
-				ninaBoton.addAction( sequence(delay(1.0f), fadeOut(0.75f)));
-				ninoBoton.addAction( sequence(delay(1.0f), fadeOut(0.75f),
-				new Action() {
-					@Override
-					public boolean act(float delta) {
-						// La ??ltima acci??n mover?? hacia pantalla de men??
-						game.setScreen(new MenuScreen(game));
-						return true;
-					}
-				}));
-			}
-		});
+		ninoBoton.setX(w / 4f - ninoBoton.getWidth() / 2f);
+		ninoBoton.setY(h / 2f - ninoBoton.getHeight() / 2f);		
+		ninoBoton.addListener(new MyClickListener("pelonino3"));
 		
 		ninaBoton.setX(w / (4f/3f) - ninaBoton.getWidth() / 2f);
 		ninaBoton.setY(h / 2 - ninaBoton.getHeight() / 2f);
-		ninaBoton.addListener(new InputListener() {
-			@Override
-			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-				Gdx.app.log(fibooGame.LOG, "Touching down on ninaBoton");
-				return true;
-			}
-			@Override
-			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-				Gdx.app.log(fibooGame.LOG, "Touching up on ninaBoton");
-				fibooGame.getPersonaje().getAvatar().setBase("pelonina3");
-				imgFondo.addAction( sequence(delay(1.0f), fadeOut(0.75f)));
-				ninoBoton.addAction( sequence(delay(1.0f), fadeOut(0.75f)));
-				ninaBoton.addAction( sequence(delay(1.0f), fadeOut(0.75f),
-				new Action() {
-					@Override
-					public boolean act(float delta) {
-						// La ??ltima acci??n mover?? hacia pantalla de men??
-						game.setScreen(new MenuScreen(game));
-						return true;
-					}
-				}));
-			}
-		});
+		ninaBoton.addListener(new MyClickListener("pelonina3"));
+		
 		stage.addActor(ninoBoton);
 		stage.addActor(ninaBoton);
+	}
+	
+	@Override
+	public void hide() {
+		super.hide();
+		fibooGame.MANAGER.unload("portada/polaroidnino2.png");
+		fibooGame.MANAGER.unload("portada/polaroidnina2.png");
+		super.dispose();
+	}
+
+	private class MyClickListener extends ClickListener {
+		
+		private String base;
+
+		public MyClickListener(String base) {
+			this.base = base;
+		}
+		
+		@Override
+		public void clicked(InputEvent event, float x, float y) {
+			Gdx.app.log("ChooseScreen", "Clickeando en " + base);
+			fibooGame.getPersonaje().getAvatar().setBase(base);
+			stage.addAction(sequence(delay(1.0f), fadeOut(0.75f),
+			new Action() {
+				@Override
+				public boolean act(float delta) {
+					game.setScreen(new MenuScreen(game));
+					return true;
+				}
+			}));
+		}
 	}
 
 }
