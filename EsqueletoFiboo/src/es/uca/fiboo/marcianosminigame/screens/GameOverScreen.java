8c38b035d2cf397530ad17257f3d5ee2f7421318
ismael.jerezibanez@gmail.com <ismael.jerezibanez@gmail.com@15079975-0d8f-49b9-43ae-de39589eff96>
2014-01-15 22:07:39 +0000

    Añadido return en algunas pantallas que no lo tenian.

--- EsqueletoFiboo/src/es/uca/fiboo/marcianosminigame/screens/GameOverScreen.java
+++ EsqueletoFiboo/src/es/uca/fiboo/marcianosminigame/screens/GameOverScreen.java
@@ -1,6 +1,9 @@
 package es.uca.fiboo.marcianosminigame.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
@@ -34,6 +37,21 @@ public class GameOverScreen extends AbstractScreen {
 
 	@Override
 	public void show() {
+		InputMultiplexer inputMultiplexer = new InputMultiplexer(new InputAdapter() {
+			@Override
+			public boolean keyUp(int keycode) {
+				if (keycode == Keys.BACK || keycode == Keys.ESCAPE){
+					//musicaFondo.stop();
+					//fibooGame.MANAGER.get("sonidos/fondo.mp3", Sound.class).loop();
+					game.setScreen(new MenuMiniJuegosScreen(game));
+					dispose();
+				}
+				return false;
+			}
+		}, stage);
+		
+		Gdx.input.setInputProcessor(inputMultiplexer);
+		
 		time = 0;
 		gameover= new Image(fibooGame.MANAGER.get("marcianosminigame/gameover.png", Texture.class));
 		gameover.setFillParent(true);
