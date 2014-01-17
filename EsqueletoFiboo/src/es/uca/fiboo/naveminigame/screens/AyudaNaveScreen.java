4357e66a526d11fa4d3d8f389ab749094d8519a7
Sergiovb.Inf@gmail.com <Sergiovb.Inf@gmail.com@15079975-0d8f-49b9-43ae-de39589eff96>
2014-01-17 12:50:23 +0000

    Ayuda y play en una.

--- EsqueletoFiboo/src/es/uca/fiboo/naveminigame/screens/AyudaNaveScreen.java
+++ EsqueletoFiboo/src/es/uca/fiboo/naveminigame/screens/AyudaNaveScreen.java
@@ -1,20 +1,26 @@
 package es.uca.fiboo.naveminigame.screens;
 
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.delay;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.fadeOut;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.sequence;
+
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.GL11;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 import es.uca.fiboo.fibooGame;
 import es.uca.fiboo.screens.AbstractScreen;
 
 public class AyudaNaveScreen extends AbstractScreen {
 
-	private Image pantallaAyuda;
-	private Image imgFondo;
+	private Image playBoton;
 	private NinePatch loaderVacio;
 	private NinePatch loaderFull;
 	private float w, h;
@@ -25,7 +31,16 @@ public class AyudaNaveScreen extends AbstractScreen {
 		//fibooGame.MANAGER.get("sonidos/ayuda.mp3",Sound.class).loop();
 		Gdx.input.setInputProcessor(stage);
 		w = Gdx.graphics.getWidth();
-		h = Gdx.graphics.getHeight();	
+		h = Gdx.graphics.getHeight();
+		
+		Texture vacioT = fibooGame.MANAGER.get("loading/vacio.png", Texture.class);
+		Texture fullT = fibooGame.MANAGER.get("loading/full.png", Texture.class);
+		loaderVacio = new NinePatch(new TextureRegion(vacioT, 24, 24), 8, 8, 8, 8);
+		loaderFull = new NinePatch(new TextureRegion(fullT, 24, 24), 8, 8, 8, 8);
+		
+		Texture boton = fibooGame.MANAGER.get("portada/playportada.png", Texture.class); 
+        boton.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+        playBoton = new Image(boton);
 	}
 	
 	@Override
@@ -36,21 +51,40 @@ public class AyudaNaveScreen extends AbstractScreen {
 		fondo.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 		ayuda.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 		
-		imgFondo = new Image(fondo);
+		Image imgFondo = new Image(fondo);
 		imgFondo.setFillParent(true);
 		
-		pantallaAyuda = new Image(ayuda);
+		Image pantallaAyuda = new Image(ayuda);
 		pantallaAyuda.setSize(Gdx.graphics.getWidth()*0.85f, Gdx.graphics.getWidth()*0.85f);
 		pantallaAyuda.setX(Gdx.graphics.getWidth()/2 - pantallaAyuda.getWidth()/2); 
         pantallaAyuda.setY(Gdx.graphics.getHeight()/2 - pantallaAyuda.getHeight()/2);
 		
+        float botonWidth = w * 0.3f;
+		float botonHeight = botonWidth;
+		
+		playBoton.setSize(botonWidth, botonHeight);
+        playBoton.setX(w/2 - playBoton.getWidth()/2);
+        playBoton.setY(h/6 - playBoton.getHeight()/2);
+        playBoton.setVisible(false);
+        
+        playBoton.addListener(new ClickListener() {
+            @Override
+			public void clicked(InputEvent event, float x, float y) {
+				stage.addAction(sequence(delay(0.5f), fadeOut(0.75f),
+                    new Action() {
+                        @Override
+                        public boolean act(float delta) {
+                        	dispose();
+                        	setGameScreen();
+                            return true;
+                        }
+                    }));
+			}
+        });
+        
         stage.addActor(imgFondo);
 		stage.addActor(pantallaAyuda);
-		
-		Texture vacioT = fibooGame.MANAGER.get("loading/vacio.png", Texture.class);
-		Texture fullT = fibooGame.MANAGER.get("loading/full.png", Texture.class);
-		loaderVacio = new NinePatch(new TextureRegion(vacioT, 24, 24), 8, 8, 8, 8);
-		loaderFull = new NinePatch(new TextureRegion(fullT, 24, 24), 8, 8, 8, 8);
+		stage.addActor(playBoton);
 		
 		//Cargando sonidos
 		fibooGame.MANAGER.loadNaveMiniGameSounds();
@@ -59,27 +93,33 @@ public class AyudaNaveScreen extends AbstractScreen {
 	@Override
 	public void render(float delta) {
 		Gdx.gl.glClearColor(0, 0, 0, 1f);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		Gdx.gl.glClear(GL11.GL_COLOR_BUFFER_BIT);
 		
 		stage.draw();
 	
-		float progress = fibooGame.MANAGER.getProgress();
-		batch.begin();
-		
-		loaderVacio.draw(batch, w*0.375f, h/8 - h*0.025f, w/4, h*0.05f);
-		if(progress > 0.05f) {
-			loaderFull.draw(batch, w*0.375f, h/8 - h*0.025f, progress*(w/4), h*0.05f);
-		}
-		
-		batch.end();
-		Gdx.app.log("InicioNaveMiniGameScreen", "Cargado al: " + progress + "%");
-		
 		if(fibooGame.MANAGER.update()) {
-			game.setScreen(new AyudaPlayNaveScreen(game));
-			dispose();
+			if(!playBoton.isVisible()) {
+				playBoton.setVisible(true);
+			}
 		}
-				
+		else {
+			float progress = fibooGame.MANAGER.getProgress();
+			
+			batch.begin();
+			loaderVacio.draw(batch, w*0.375f, h/8 - h*0.025f, w/4, h*0.05f);
+			if(progress > 0.05f) {
+				loaderFull.draw(batch, w*0.375f, h/8 - h*0.025f, progress*(w/4), h*0.05f);
+			}
+			batch.end();
+			Gdx.app.log(getName(), "Cargado al: " + progress + "%");
+		}
+						
 		stage.act(delta);
+	}
+	
+
+	private void setGameScreen() {
+        game.setScreen(new NaveMiniGameScreen(game));
 	}
 
 }
